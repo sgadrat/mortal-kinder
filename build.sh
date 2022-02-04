01a0793acebd6870e18360bd9bb43f05cf6979e4
Sylvain Gadrat <sgadrat@wontfix.it>
2022-02-04 13:22:07 +0100

    rename to Mortal Kinder

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -e
 asm="${NAKEN_BIN:-naken_asm}"
 
 tools/build_sprites.py data/spritesheets/dora.png > data/sprites.built.bin
-"$asm" -l -type bin mortal_kuties.asm -o mortal_kuties.bin
+"$asm" -l -type bin mortal_kinder.asm -o mortal_kinder.bin
